9f414496ce4c0a8d3027e69e85c79b81d16a2703
Jhonatas-Anthony <jhonatasanthony6@gmail.com>
2023-07-17 22:57:04 -0300

    criação de função para realizar uam venda

--- data/inserts/table.sql
+++ data/inserts/table.sql
@@ -8,13 +8,19 @@ CREATE TABLE IF NOT EXISTS "infos" (
   "id" serial PRIMARY KEY ,
   "product_id" integer,
   "supplier_id" integer,
-  "lote" varchar,
-  "validity" varchar,
+  "validity" date,
   "purchace_price" float,
   "internal_code" varchar
 );
 
-CREATE TABLE IF NOT EXISTS "notaFiscal" (
+CREATE TABLE IF NOT EXISTS "deposit" (
+  "product_id" integer,
+  "quanty" integer,
+  "lote" date,
+  PRIMARY KEY(product_id, lote)
+);
+
+CREATE TABLE IF NOT EXISTS "nota_fiscal" (
   "id" serial PRIMARY KEY ,
   "product_id" integer,
   "quanty" integer,
@@ -52,19 +58,20 @@ CREATE TABLE IF NOT EXISTS "offices" (
   "salary" float
 );
 
-CREATE TABLE IF NOT EXISTS "notaFiscal_products" (
+/* CREATE TABLE IF NOT EXISTS "notaFiscal_products" (
   "notaFiscal_product_id" integer,
   "products_id" integer,
   PRIMARY KEY ("notaFiscal_product_id", "products_id")
-);
+); */
 
 -- Adicionando chaves estrangeiras
 
 ALTER TABLE "infos" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
 ALTER TABLE "infos" ADD FOREIGN KEY ("supplier_id") REFERENCES "supliers" ("id");
-ALTER TABLE "notaFiscal" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+ALTER TABLE "nota_fiscal" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
 ALTER TABLE "sales" ADD FOREIGN KEY ("nota_id") REFERENCES "notaFiscal" ("id");
 ALTER TABLE "sales" ADD FOREIGN KEY ("employees_id") REFERENCES "employees" ("id");
+ALTER TABLE "deposit" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
 ALTER TABLE "employees" ADD FOREIGN KEY ("office_id") REFERENCES "offices" ("id");
-ALTER TABLE "notaFiscal_products" ADD FOREIGN KEY ("notaFiscal_product_id") REFERENCES "notaFiscal" ("id");
-ALTER TABLE "notaFiscal_products" ADD FOREIGN KEY ("products_id") REFERENCES "products" ("id");
+/* ALTER TABLE "notaFiscal_products" ADD FOREIGN KEY ("notaFiscal_product_id") REFERENCES "notaFiscal" ("id");
+ALTER TABLE "notaFiscal_products" ADD FOREIGN KEY ("products_id") REFERENCES "products" ("id"); */
